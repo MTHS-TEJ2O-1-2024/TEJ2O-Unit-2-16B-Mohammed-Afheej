06833fcc3e3e88f3e05a8a1d3e5c26a0f3c9cd6c
Mohammed-Afheej <mohammed.afheej@mths.ca>
2024-11-07 11:21:05 -0500

    edited the comment

--- main.ts
+++ main.ts
@@ -1,8 +1,32 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Mohammed Afheej
+ * Created on: November 2024
+ * This program 
 */
+// setup
+radio.setGroup(91)
+let distanceOfObject: number
+basic.showIcon(IconNames.Happy)
 
-basic.showString('Hello, World!')
+input.onButtonPressed(Button.A, function () {
+    //distanceOfObject
+    distanceOfObject = sonar.ping(
+        DigitalPin.P1,
+        DigitalPin.P2,
+        PingUnit.Centimeters
+    )
+
+    // if distance is greater than 10
+    if (distanceOfObject > 10) {
+        basic.showIcon(IconNames.Triangle)
+        radio.sendString("To Close")
+        basic.showIcon(IconNames.Happy)
+    }
+})
+
+radio.onReceivedString(function (receivedString) {
+    basic.clearScreen()
+    basic.showString(receivedString)
+    basic.showIcon(IconNames.Happy)
+})
